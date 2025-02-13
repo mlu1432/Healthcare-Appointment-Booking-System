7443cc9d37371046b8369069437e29a61834e3d5
Lucas Sekwati <Luxturesekwati@gmail.com>
2025-02-13 22:20:56 +0200

    update

--- firstcare-frontend/next.config.mjs
+++ firstcare-frontend/next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['fonts.googleapis.com', 'fonts.gstatic.com'],
+    },
+    staticPageGenerationTimeout: 120,
+    output: 'standalone',
+};
 
 export default nextConfig;
